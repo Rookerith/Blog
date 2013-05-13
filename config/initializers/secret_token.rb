dc5ac45305d591637dbe6ee1197ecded431ef8a7
Jason Janson <rookerith@gmail.com>
2013-05-13 15:22:20 -0400

    Ran new site: 'rails new C:\Sites\WireCare --skip-test-unit --skip-bundle', then updated gems to include bootstrap, haml, rspec and cucumber related testing gems.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '677c7329ff3a670d962005b1d7937d49613d015a6fef57a6cc23cd6425883c189270af05feb208082aa7adb5f9d1fa3b95fcdce22affa93b218f0c189bdaf76f'
